5441e06a959768cc8fe5430b275ce75210a45e8e
TriGirlTiff <ls13tr@gmail.com>
2017-07-23 13:52:16 -0700

    added solve option, win/lose message

--- disney_hangman.py
+++ disney_hangman.py
@@ -1,11 +1,12 @@
 import random
 
 dl_words = [
-"FRONTIERLAND",
-"FANTASYLAND",
-"ADVENTURELAND",
-"TOMORROWLAND",
-"CHURRO",
+# "FRONTIERLAND",
+# "FANTASYLAND",
+# "ADVENTURELAND",
+# "TOMORROWLAND",
+# "CHURRO",
+"HAUNTED MANSION"
 ]
 # h_word = random.choice(dl_words)
 good_guesses =[]
@@ -22,12 +23,17 @@ def greeting():
 def rules():
     """Prints rules of game"""
     print 
-    print "These are the rules of the game. You can miss 4 letters or 2 solve attempts"
+    print "These are the rules of the game. You can miss 4 letters or one solve attempt."
 
-def player_options():
+def should_guess_word():
     """Player chooses to enter another letter or solve"""
-    user_choice = raw_input ("\n\nEnter 1 to enter a letter or 2 to solve the puzzle. ")
-    return user_choice
+    user_choice = raw_input ("\n\nPress enter to guess a letter or 2 to solve the puzzle. ")
+    if user_choice == "2":
+        return True
+    else:
+        return False
+
+
 
 
 def get_word(dl_words):
@@ -37,17 +43,28 @@ def get_word(dl_words):
 
 
 
-def get_spaces(h_word):
-    """Shows number of spaces in hangman word"""
-    h_word = get_word(dl_words)
-    for letters in h_word:
-        print "_ ", 
+# def get_spaces(h_word):
+#     """Shows number of spaces in hangman word"""
+#     h_word = get_word(dl_words)
+#     for letters in h_word:
+#         print "_ ", 
 
 def solve_guess(h_word):
     user_solve = raw_input ("What is the hangman word? ")
-    return user_solve
+    user_solve = user_solve.upper()
+    if user_solve == h_word:
+        print "You win! {} was the word!".format(h_word)
+    else:
+        print "Sorry,{} is not the hangman word".format(user_solve)
 
-
+def check_win(h_word, good_guesses):
+    for letter in h_word:
+        if letter in good_guesses or letter == " ":
+            continue
+        else:
+            return False
+    return True
+    
 
 def get_letter():
     """Gets a letter from the user"""
@@ -60,34 +77,50 @@ def display_word(h_word):
     for letter in h_word:
         if letter in good_guesses:
             print letter,
+        elif letter == " ":
+            print letter,
         else:
             print "_",
-
+    print 
 
 greeting()
 rules()
-player_options()
+# player_options()
 h_word = get_word(dl_words)
-get_spaces(h_word)
+display_word(h_word)
+# get_spaces(h_word)
 
 
 
 
 num_bad_guesses = 0
 while num_bad_guesses < 4:
-    user_guess = get_letter()
-    if user_guess in bad_guesses or user_guess in good_guesses:
-        bad_guesses.append(user_guess)
-        good_guesses.append(user_guess)
-        print "You have already guessed {}, guess again".format(user_guess)
-    elif user_guess not in h_word:
-        bad_guesses.append(user_guess)
-        num_bad_guesses += 1
-        print "{} isn't in the word, guess again".format(user_guess)
-    elif user_guess in h_word:
-        good_guesses.append(user_guess)
-        print "Good guess!"
+    if should_guess_word():
+        solve_guess(h_word)
+        break
+    
+    else:
+        user_guess = get_letter()
+        if user_guess in bad_guesses or user_guess in good_guesses:
+            bad_guesses.append(user_guess)
+            good_guesses.append(user_guess)
+            print "You have already guessed {}, guess again".format(user_guess)
+        
+        elif user_guess not in h_word:
+            bad_guesses.append(user_guess)
+            num_bad_guesses += 1
+            print "{} isn't in the word, guess again".format(user_guess)
+        
+        elif user_guess in h_word:
+            good_guesses.append(user_guess)
+            print "Good guess!"
+            if check_win(h_word, good_guesses):
+                print "You win!"
+                break
 
     display_word(h_word)
+
+    if num_bad_guesses == 4:
+        print "You ran out of chances! The hangman word was {}".format(h_word)
 
 
